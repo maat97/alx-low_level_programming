98fb2379c65f36a5b6b73716612838f3c45c5376
maat97 <m14597@icloud.com>
2023-07-17 15:01:32 -0700

    My first commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,8 +20,8 @@ int main(void)
 			digit3 = 0;
 			while (digit3 <= 9)
 			{
-			if (digit1 != digit2 && digit1 < digit2 &&
-					digit2 != digit3 && digit2 < digit3)
+				if (digit1 != digit2 && digit1 < digit2 &&
+				digit2 != digit3 && digit2 < digit3)
 				{
 					putchar(digit1 + 48);
 					putchar(digit2 + 48);
@@ -31,14 +31,10 @@ int main(void)
 						putchar(',');
 						putchar(' ');
 					}
-				}
-				digit3++;
-			}
-			digit2++;
-		}
-		digit1++;
+				} digit3++;
+			} digit2++;
+		} digit1++;
 	}
 	putchar('\n');
-
 	return (0);
 }
