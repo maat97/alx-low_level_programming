fcf5269e7936c9d18bdcd21595f2eb48a618cb6c
maat97 <m14597@icloud.com>
2023-07-24 18:29:44 -0700

    a

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - generates random password for 101- crackme
+ * Return: zero
+*/
+
+int main(void)
+{
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
