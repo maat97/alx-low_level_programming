faffdb1e00d1d2f5bff484da3ab811b57307c97d
maat97 <m14597@icloud.com>
2023-07-27 20:11:16 -0700

    a

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program prints a[2] = 98, followed by a new line
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	/**
+	 * write  your new line of code
+	 * You are not allowed to use the variable a
+	 * You are not allowed to modify the variable p
+	 * You can only write one statement
+	 * You are not allowed to code anything else
+	 * than the line of expected line of code
+	 */
+	*(p + 5) = 98;
+	/* so that print 98 */
+	printf("a[2] = %d/n", a[2]);
+	return (0);
+}
