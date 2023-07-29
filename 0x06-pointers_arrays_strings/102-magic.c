f07e5bed44df153cb1ac5b2ddd637fccb9771ebd
maat97 <m14597@icloud.com>
2023-07-29 07:56:32 -0700

    a

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -10,9 +10,12 @@
 
 int main(void)
 {
+	int n;
 	int a[5];
+	int *p;
 
 	a[2] = 1024;
+	p = &n;
 
 	/**
 	 * write  your new line of code
@@ -23,9 +26,9 @@ int main(void)
 	 * than the line of expected line of code
 	 */
 
-	/* so that print 98 */
-	
-	printf("a[2] = 98\n" + (2 * 4));
+	(void)(p == a && (*p = 98));
 
+	/* so that print 98 */
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
